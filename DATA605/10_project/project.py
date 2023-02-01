7254bb2b536ac28923998b12efa95bbe465b7d57
avoytkiv <avoytkiv@gmail.com>
2023-02-01 11:55:30 -0700

    Remove unused code

--- DATA605/10_project/project.py
+++ DATA605/10_project/project.py
@@ -4,18 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
 
-# Read in the data with no header
-df = pd.read_csv('/Users/berg/DataspellProjects/MDSA-UofC/DATA605/10_project/data/portable_user_history-2023-01-30-csv.csv', header=None)
-# Check the data
-df.head()
-df.info()
-df.describe()
-# Check for missing values
-df.isnull().sum()
-# Check for duplicates
-df.duplicated().sum()
-# Convert the date column to datetime
-df['date'] = pd.to_datetime(df['Date'])
-# Difference in days between the first and last date
-time_range = df['date'].max() - df['date'].min()
+# Read in the data with first row as header
+df = pd.read_csv('/Users/berg/DataspellProjects/MDSA-UofC/DATA605/10_project/data/rescutime_hourly_cleaned_1.csv', header=0, index_col=0)
+
 
